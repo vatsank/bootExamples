9485a2b1431e61ec98495fa0e30d21bb6a319711
Srivatsan <'vatsank@gmail.com'>
2019-03-08 23:21:54 +0530

    Changed Controller Package

--- driverProfileConsumer/src/main/java/com/example/demo/controllers/FindPartnerController.java
+++ driverProfileConsumer/src/main/java/com/example/demo/controllers/FindPartnerController.java
@@ -1,4 +1,4 @@
-package com.example.controllers;
+package com.example.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
